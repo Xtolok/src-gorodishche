96613e8ae78c678722d14c3da5e9f4be64976eaa
Dir <dir_61@bk.ru>
2026-06-08 23:42:22 +0300

    dsdds

--- config/bootstrap.py
+++ config/bootstrap.py
@@ -12,3 +12,21 @@ def bootstrap_render():
 
     if not Page.objects.filter(slug=Page.SLUG_INDEX).exists():
         call_command('loaddata', 'initial_data', verbosity=0)
+
+    _ensure_superuser()
+
+
+def _ensure_superuser():
+    username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    if not username or not password:
+        return
+
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+    if User.objects.filter(username=username).exists():
+        return
+
+    email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+    User.objects.create_superuser(username, email, password)
