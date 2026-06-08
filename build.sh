be4453d3619d7d40bc3a4b54bee13f85450da859
Dir <dir_61@bk.ru>
2026-06-08 23:18:33 +0300

    dsdds

--- build.sh
+++ build.sh
@@ -3,9 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py migrate --noinput
-python manage.py populate_site
-python manage.py import_about_volganet
-python manage.py import_legal_volganet
-python manage.py import_current_volganet
-python manage.py import_nok_volganet
+python manage.py loaddata initial_data
 python manage.py collectstatic --noinput
